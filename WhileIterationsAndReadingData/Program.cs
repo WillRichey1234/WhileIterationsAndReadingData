ee249b80b8753c00a33353e15b14138d79d27998
WillRichey1234 <gsampson@vms.edu>
2017-10-09 13:46:36 -0600

    While Iterations Homework

---
+++ WhileIterationsAndReadingData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace WhileIterationsAndReadingData
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("Values.txt");
+            string line = "";
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null){
+                    Console.WriteLine(line);
+                }
+            }
+            myReader.Close();
+            Console.ReadLine();
+        }
+    }
+}
